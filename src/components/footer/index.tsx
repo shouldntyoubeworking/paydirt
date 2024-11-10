9f07e0964590126b756f8005d7c54e6f8d0dc7b7
WatchOutIGotYou <185667804+shouldntyoubeworking@users.noreply.github.com>
2024-11-09 23:19:30 -0800

    Footer date fix

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,7 +3,7 @@
 export default function Footer(){
     return <div className="footer">
         <div className="footer-items">
-            <div className="footer-item-data">Data as of Nov 3, 2024</div>
+            <div className="footer-item-data">Data as of Nov 9, 2024</div>
             <div className="footer-item-data"><a href="https://shouldntyoubeworking.com/">shouldntyoubeworking?</a></div>
             <div className="footer-item-model-number">System 0.02 (🤡)</div>
         </div>
